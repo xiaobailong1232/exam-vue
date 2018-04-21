012c457f5f0a1ff17a5794e2ab5dbe5b38279237
Baron <282584778@qq.com>
2018-04-21 15:51:56 +0800

    增加前进 和 后退按钮

--- src/views/layout/AppMain.vue
+++ src/views/layout/AppMain.vue
@@ -1,9 +1,13 @@
 <template>
-  <section class="app-main">
-    <transition name="fade" mode="out-in">
-      <router-view :key="key"></router-view>
-    </transition>
-  </section>
+    <section class="app-main">
+      <div style="margin:5px 0 0 20px;">
+        <el-button @click="prev" size="mini">&lt;</el-button>
+        <el-button @click="next" size="mini">&gt;</el-button>
+      </div>
+      <transition name="fade" mode="out-in">
+        <router-view :key="key"></router-view>
+      </transition>
+    </section>
 </template>
 
 <script>
@@ -12,6 +16,14 @@ export default {
   computed: {
     key() {
       return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
+    }
+  },
+  methods: {
+    prev() {
+      this.$router.go(-1)
+    },
+    next() {
+      this.$router.go(1)
     }
   }
 }
